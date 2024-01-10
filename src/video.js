e96ea6d0dcc97efed49238f930099cfe9cf3d5be
Robert Hendricks <rmasonh22@gmail.com>
2024-01-09 21:02:41 -0500

    add assets include audio,image, video

--- src/video.js
+++ src/video.js
@@ -1,11 +1,28 @@
 // npm install react-player
+// https://github.com/cookpete/react-player
 
 import React from 'react';
 import ReactPlayer from 'react-player/youtube';
 
+// move url into props
+
+
+
 export default function Video() {
+    const vidUrl="https://www.youtube.com/watch?v=EmOL5uwuHhM";
+
     return (
-        <ReactPlayer url = "https://www.youtube.com/watch?v=EmOL5uwuHhM" />
+        <>
+        <div className="video">
+        <ReactPlayer
+        url =  {vidUrl}
+        playing={false}
+        controls={true}
+        volume={.5}
+        />
+        </div>
+        </>
+       
     );
 
 }
